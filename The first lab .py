6a98240164dfa3df0f0cd5dc90524b4e2a06a269
Egyfares <Egyfares2009@hotmail.com>
2023-03-07 18:48:14 +0200

    Update The first lab .py

--- The first lab .py
+++ The first lab .py
@@ -8,9 +8,9 @@ from PIL import Image
 # open images
 
 for i in os.listdir("images/"):
-    if i != '.DS_Store':
+    if i != '.DS_Store': # to ensure the onle image files
         im = Image.open(os.path.join("images/",i))
-        im = im.rotate(-90)
+        im = im.rotate(-90) # or im = im.rotate(270) 360-90
         im = im.resize((128,128))
         im = im.convert("RGB")
         im.save (os.path.join("/opt/icons/", i + ".jpeg"))
